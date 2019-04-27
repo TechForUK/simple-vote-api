0ec4c85aa7a02c8a92ca58229ad73ecfc7e9ec57
Robert Munro <rob@sentinelweb.co.uk>
2019-04-27 18:03:12 +0100

    read params

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+app.use(bodyParser.json());
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
