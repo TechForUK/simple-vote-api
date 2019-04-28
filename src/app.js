5cfcca506fd0f15d6baa8362a8270cd79b5214d7
Phil <phil@operatedata.com>
2019-04-28 13:30:34 +0100

    no message

--- src/app.js
+++ src/app.js
@@ -14,11 +14,18 @@ app.get('/', (req, res) => res.send('Hello World!'));
 
 app.post('/register', (req, res) => {
   const { userData } = req.body;
+  //userData.
   const basicFormPdf = signBasicForm(userData);
   const postalFormPdf = signPostalForm(userData);
   const euFormPdf = signEuForm(userData);
   sendEmail([basicFormPdf,postalFormPdf, euFormPdf],userData.toEmail, userData.fromEmail, userData.firstName + ' ' + userData.surname);
   res.sendStatus(200);
 });
+
+// `uk_citizen_in_uk`
+// `eu_citizen_in_uk`
+// `uk_citizen_abroad`
+
+//PLUS POSTAL FORM
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
