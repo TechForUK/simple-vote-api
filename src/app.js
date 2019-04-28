6ae8c4f49ac858fe82a3d2c538886bf8ee2c6787
Phil <phil@operatedata.com>
2019-04-28 18:35:39 +0100

    Added validation to app.js file

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const signBasicForm = require('./signBasicForm.js');
 const signPostalForm = require('./signPostalForm.js');
 const signEuForm = require('./signEuForm.js');
 const sendEmail = require('./sendEmail.js');
+const validation = require('./validation.js');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,27 +15,31 @@ app.get('/', (req, res) => res.send('Hello World!'));
 
 app.post('/register', (req, res) => {
   const { userData } = req.body;
-  const pdfDocuments = [];
 
-  switch(userData.userType) {
-    case 'uk_citizen_in_uk':
-      pdfDocuments.push(signBasicForm(userData));
-      break;
-    case 'eu_citizen_in_uk':
-      pdfDocuments.push(signBasicForm(userData));
-      pdfDocuments.push(signEuForm(userData));
-      break;
-    // case 'uk_citizen_abroad':
-    //   pdfDocuments.push(signBasicForm(userData));
-    //   break;
-  }
-  if (userData.reqiresPostalForm){
-      pdfDocuments.push(signPostalForm(userData));
-  }
+  if (validation(userData))
+  {
+      const pdfDocuments = [];
 
-  sendEmail(pdfDocuments,userData.toEmail, userData.fromEmail, userData.firstName + ' ' + userData.surname);
+      switch(userData.userType) {
+        case 'uk_citizen_in_uk':
+          pdfDocuments.push(signBasicForm(userData));
+          break;
+        case 'eu_citizen_in_uk':
+          //pdfDocuments.push(signBasicForm(userData));
+          //pdfDocuments.push(signEuForm(userData));
+          break;
+        // case 'uk_citizen_abroad':
+        //   pdfDocuments.push(signBasicForm(userData));
+        //   break;
+      }
+      if (userData.postalVote){
+          pdfDocuments.push(signPostalForm(userData));
+      }
+  }
+  //sendEmail(pdfDocuments,userData.toEmail, userData.fromEmail, userData.firstName + ' ' + userData.surname);
   res.sendStatus(200);
 });
+
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
