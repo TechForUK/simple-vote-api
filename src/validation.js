b6f34baf3f4f229bc72a0b72ff41ae68b80ba89d
Phil <phil@operatedata.com>
2019-05-01 22:30:49 +0100

    extra validation added

--- src/validation.js
+++ src/validation.js
@@ -38,6 +38,139 @@ function validation(userData){
       return false;
     }
 
+    
+
+    if (typeof userData.differentAddress !== 'boolean'){
+      console.log('differentAddress boolean check failed ' + typeof userData.differentAddress);
+      return false;
+    }
+    else if(userData.differentAddress)
+    {
+      if(userData.firstLineCurrentAddress.length > lenTextFields || typeof userData.firstLineCurrentAddress !== 'string'){
+        console.log('firstLineCurrentAddress validation failed ' + typeof userData.firstLineCurrentAddress);
+        return false;
+      }
+
+      if(userData.secondLineCurrentAddress.length > lenTextFields || typeof userData.secondLineCurrentAddress !== 'string'){
+        console.log('secondLineCurrentAddress validation failed ' + typeof userData.secondLineCurrentAddress);
+        return false;
+      }
+
+      if(userData.currentPostcode.length > lenTextFields || typeof userData.currentPostcode !== 'string'){
+        console.log('currentPostcode validation failed ' + typeof userData.currentPostcode);
+        return false;
+      }
+
+      if(userData.currentCity.length > lenTextFields || typeof userData.currentCity !== 'string'){
+        console.log('currentCity validation failed ' + typeof userData.currentCity);
+        return false;
+      }
+    }
+
+    if (typeof userData.movedHouse !== 'boolean'){
+      console.log('movedHouse boolean check failed ' + typeof userData.movedHouse);
+      return false;
+    }
+    else if(userData.movedHouse){
+      if(userData.oldAddressFirstLineAddress.length > lenTextFields || typeof userData.oldAddressFirstLineAddress !== 'string'){
+        console.log('oldAddressFirstLineAddress validation failed ' + typeof userData.oldAddressFirstLineAddress);
+        return false;
+      }
+
+      if(userData.oldAddressSecondLineAddress.length > lenTextFields || typeof userData.oldAddressSecondLineAddress !== 'string'){
+        console.log('oldAddressSecondLineAddress validation failed ' + typeof userData.oldAddressSecondLineAddress);
+        return false;
+      }
+
+      if(userData.oldAddressPostcode.length > lenTextFields || typeof userData.oldAddressPostcode !== 'string'){
+        console.log('oldAddressPostcode validation failed ' + typeof userData.oldAddressPostcode);
+        return false;
+      }
+
+      if(userData.oldAddressCity.length > lenTextFields || typeof userData.oldAddressCity !== 'string'){
+        console.log('oldAddressCity validation failed ' + typeof userData.oldAddressCity);
+        return false;
+      }
+    }
+
+
+    if (typeof userData.postalVote !== 'boolean'){
+      console.log('postalVote boolean check failed ' + typeof userData.postalVote);
+      return false;
+    }
+    else if(userData.postalVote){
+      if(userData.postalVoteOption.length > lenTextFields || typeof userData.postalVoteOption !== 'string'){
+        console.log('postalVoteOption validation failed ' + typeof userData.postalVoteOption);
+        return false;
+      }
+      else if(userData.postalVoteOption === 'indefintiely')
+      {
+        // all ok nothing else to do
+      }
+      else if(userData.postalVoteOption === 'specificElection')
+      {
+        if(validDate(userData.postalVoteElectionDate) == false){
+          console.log('currentDate validation failed');
+          return false;
+        }
+      }
+      else if(userData.postalVoteOption === 'timePeriod')
+      {
+        if(validDate(userData.postalVoteFrom) == false){
+          console.log('postalVoteFrom validation failed');
+          return false;
+        }
+        if(validDate(userData.postalVoteTo) == false){
+          console.log('postalVoteTo validation failed');
+          return false;
+        }
+      }
+      else
+      {
+        console.log('postalVoteOption unknown: ' + userData.postalVoteOption);
+        return false;
+      }
+        
+
+    }
+        
+    if(userData.previousName.length > lenTextFields || typeof userData.previousName !== 'string'){
+      console.log('previousName validation failed ' + typeof userData.previousName);
+      return false;
+    }
+
+    if(userData.citizenOf.length > lenTextFields || typeof userData.citizenOf !== 'string'){
+      console.log('citizenOf validation failed ' + typeof userData.citizenOf);
+      return false;
+    }
+
+    if(userData.nationality.length > lenTextFields || typeof userData.nationality !== 'string'){
+      console.log('nationality validation failed ' + typeof userData.nationality);
+      return false;
+    }
+
+    if (typeof userData.registeredAsOverseasVoter !== 'boolean'){
+      console.log('registeredAsOverseasVoter boolean check failed ' + typeof userData.registeredAsOverseasVoter);
+      return false;
+    }
+
+    if(userData.homeCountryConstituency.length > lenTextFields || typeof userData.homeCountryConstituency !== 'string'){
+      console.log('homeCountryConstituency validation failed ' + typeof userData.homeCountryConstituency);
+      return false;
+    }
+
+    if(userData.citizenOf.length > lenTextFields || typeof userData.citizenOf !== 'string'){
+      console.log('citizenOf validation failed ' + typeof userData.citizenOf);
+      return false;
+    }
+
+    if(userData.nationality.length > lenTextFields || typeof userData.nationality !== 'string'){
+      console.log('nationality validation failed ' + typeof userData.nationality);
+      return false;
+    }
+      
+
+
     if(validNINNumber(userData.nin) == false && userData.nin.replace(/ /g,'').length != 9){
       console.log('Nin validation failed');
       return false;
@@ -57,35 +190,7 @@ function validation(userData){
       console.log('PostalVote boolean check failed ' + typeof userData.postalVote);
       return false;
     }
-    if (typeof userData.movedHouse !== 'boolean'){
-      console.log('movedHouse boolean check failed ' + typeof userData.movedHouse);
-      return false;
-    }
 
-
-    
-    // surname: '',
-    // firstLineAddress: '',
-    // secondLineAddress: '',
-    // postcode: '',
-    // city: '',
-    // differentAddress: false,
-    // telephone: '',
-    // email: '',
-    // nationality: '',
-    // movedHouse: false,
-    // oldAddressFirstLineAddress: '',
-    // oldAddressSecondLineAddress: '',
-    // oldAddressPostcode: '',
-    // oldAddressCity: '',
-    // registeredAsOverseasVoter: false,
-    // dateOfBirth: '',
-        
-    // changedName: '',
-    // previousName: '',
-    // dateOfNameChange: '',
-        
-    // currentDate: ''
     
   }
   catch(e) {
