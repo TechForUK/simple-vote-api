40fcc8fa540b100ec2b819e173901eb591095826
Phil <phil@operatedata.com>
2019-05-03 11:48:56 +0100

    Please check this fromEmail change

--- src/sendEmail.js
+++ src/sendEmail.js
@@ -34,12 +34,12 @@ function sendEmail(files, toEmail, fromEmail, name) {
       console.log(e.message);
     });
 
-<<<<<<< HEAD
+
   // const msg2 = {
   //   to: fromEmail,
   //   from: 'no-reply@getvoting.org',
   //   subject: 'Your vote registration email was sent',
-  //   text: 'Hello,\n\nThanks a lot for using our voter registration app. The following documents were sent on your behalf to your local electoral body.\n\nHappy voting,\n\nThe Best for Britain team',
+  //   text: 'Hello,\n\nThanks a lot for using our voter registration app. The following documents were sent on your behalf to your local electoral body.\nIf you don\'t hear back from your electoral office within a day please forward them these documents manually.\n\nHappy voting,\n\nThe Best for Britain team',
   //   attachments: attach,
   // };
 
@@ -50,23 +50,7 @@ function sendEmail(files, toEmail, fromEmail, name) {
   //     console.log('Failed to send email');
   //     console.log(e.message);
   //   });
-=======
-  const msg2 = {
-    to: fromEmail,
-    from: 'no-reply@getvoting.org',
-    subject: 'Your vote registration email was sent',
-    text: 'Hello,\n\nThanks a lot for using our voter registration app. The following documents were sent on your behalf to your local electoral body.\nIf you don\'t hear back from your electoral office within a day please forward them these documents manually.\n\nHappy voting,\n\nThe Best for Britain team',
-    attachments: attach,
-  };
 
-  sgMail.send(msg2)
-    .then(function(){
-      console.log('Email sent successfully');
-    }).catch(function(e){
-      console.log('Failed to send email');
-      console.log(e.message);
-    });
->>>>>>> 8d14aa8984d0535bbf3ab83bda3eef30641f6a1f
 
 }
 
