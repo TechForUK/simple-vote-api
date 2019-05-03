23ab3dd93265286b504f7157af145e9c6e3316a6
Phil <phil@operatedata.com>
2019-05-03 11:44:08 +0100

    Please check this and make sure the text is adapted

--- src/sendEmail.js
+++ src/sendEmail.js
@@ -16,7 +16,9 @@ function sendEmail(files, toEmail, fromEmail, name) {
 
   const msg = {
     to: toEmail,
-    from: fromEmail,
+    from: { email : 'register@simple.getvoting.org' , name: name},
+    reply_to: fromEmail,
+    cc: fromEmail,
     subject: 'Please add me to the electoral register',
     text: 'Hello,\n\nPlease add me to the elecoral register. I\'ve attached all the relevant forms.\n\nBest regards,\n\n'+name,
     attachments: attach,
@@ -32,21 +34,21 @@ function sendEmail(files, toEmail, fromEmail, name) {
       console.log(e.message);
     });
 
-  const msg2 = {
-    to: fromEmail,
-    from: 'no-reply@getvoting.org',
-    subject: 'Your vote registration email was sent',
-    text: 'Hello,\n\nThanks a lot for using our voter registration app. The following documents were sent on your behalf to your local electoral body.\n\nHappy voting,\n\nThe Best for Britain team',
-    attachments: attach,
-  };
+  // const msg2 = {
+  //   to: fromEmail,
+  //   from: 'no-reply@getvoting.org',
+  //   subject: 'Your vote registration email was sent',
+  //   text: 'Hello,\n\nThanks a lot for using our voter registration app. The following documents were sent on your behalf to your local electoral body.\n\nHappy voting,\n\nThe Best for Britain team',
+  //   attachments: attach,
+  // };
 
-  sgMail.send(msg2)
-    .then(function(){
-      console.log('Email sent successfully');
-    }).catch(function(e){
-      console.log('Failed to send email');
-      console.log(e.message);
-    });
+  // sgMail.send(msg2)
+  //   .then(function(){
+  //     console.log('Email sent successfully');
+  //   }).catch(function(e){
+  //     console.log('Failed to send email');
+  //     console.log(e.message);
+  //   });
 
 }
 
